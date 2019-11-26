72d5f4211d97f0f2e5924084f13d49ec35f496b6
SongJuhee <21700380@handong.edu>
2019-11-26 22:12:28 +0900

    drone

---
+++ build/CMakeFiles/TelloPro.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/telloproject-SongJuhee/src/back.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/back.cpp.o"
+  "/data/telloproject-SongJuhee/src/ccw.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/ccw.cpp.o"
+  "/data/telloproject-SongJuhee/src/cw.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/cw.cpp.o"
+  "/data/telloproject-SongJuhee/src/down.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/down.cpp.o"
+  "/data/telloproject-SongJuhee/src/flip.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/flip.cpp.o"
+  "/data/telloproject-SongJuhee/src/forward.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/forward.cpp.o"
+  "/data/telloproject-SongJuhee/src/land.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/land.cpp.o"
+  "/data/telloproject-SongJuhee/src/left.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/left.cpp.o"
+  "/data/telloproject-SongJuhee/src/python_interface.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/python_interface.cpp.o"
+  "/data/telloproject-SongJuhee/src/right.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/right.cpp.o"
+  "/data/telloproject-SongJuhee/src/takeoff.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/takeoff.cpp.o"
+  "/data/telloproject-SongJuhee/src/up.cpp" "/data/telloproject-SongJuhee/build/CMakeFiles/TelloPro.dir/src/up.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.6m"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
